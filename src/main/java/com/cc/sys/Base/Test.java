4a0d943aaf54b4cfc479c0b54dc1782e0b098185
陈诚 <chencheng0823@hupu.com>
2021-09-27 15:43:02 +0800

    合并提交

---
+++ src/main/java/com/cc/sys/Base/Test.java
@@ -0,0 +1,21 @@
+package com.cc.sys.Base;
+
+/**
+ * @Author: cc
+ * @Date: 2021/9/27 11:39 上午
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+
+    public void show_1() {
+        System.out.println("第二次提交");
+    }
+
+    public void show2() {
+        System.out.println("我是陈哈哈");
+    }
+
+}
---
+++ src/main/java/com/cc/sys/Base/User.java
@@ -0,0 +1,21 @@
+package com.cc.sys.Base;
+
+/**
+ * @Author: cc
+ * @Date: 2021/9/27 1:49 下午
+ */
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("第一次");
+    }
+
+    public void show() {
+        System.out.println("我的用户信息");
+    }
+
+    public void show2() {
+        System.out.println("我是陈哈哈");
+    }
+
+}
