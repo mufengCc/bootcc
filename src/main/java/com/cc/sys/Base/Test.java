02cd526617dea921115d31c2742d594d09a4dd63
陈诚 <chencheng0823@hupu.com>
2021-09-27 11:40:05 +0800

    第一次提交

---
+++ src/main/java/com/cc/sys/Base/Test.java
@@ -0,0 +1,12 @@
+package com.cc.sys.Base;
+
+/**
+ * @Author: cc
+ * @Date: 2021/9/27 11:39 上午
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
