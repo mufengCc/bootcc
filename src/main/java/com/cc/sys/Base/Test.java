aa017116c006186c085bd0c328b72e852526e5b3
“mufengCc” <chencheng633@163.com>
2021-09-27 13:39:41 +0800

    第二次提交

--- src/main/java/com/cc/sys/Base/Test.java
+++ src/main/java/com/cc/sys/Base/Test.java
@@ -9,4 +9,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
     }
+
+    public void show_1() {
+        System.out.println("第二次提交");
+    }
 }
